cfc3f538f1db8a964775b4dac95b13708113f877
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-02 13:04:03 +0100

    create save and reset session ruote

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+import config
 
 app = Flask(__name__)
+app.config.from_object(config)
+
+PLACEHOLDER_CODE = "print('Hello, World!')"
 
 
 @app.route("/", methods=["GET"])
 def code():
+    if session.get("code") is None:
+        session["code"] = PLACEHOLDER_CODE
     context = {
-        "message": "I'm hungry for your python code!",
+        "message": "Paste Your Python Code 🐍",
+        "code": session["code"],
     }
     return render_template("code_input.html", **context)
 
 
+@app.route("/save_code", methods=["POST"])
+def save_code():
+    session["code"] = request.get("code")
+    return redirect(url_for("code"))
+
+
+@app.route("/reset_session", methods=["POST"])
+def reset_sesion():
+    session.clear()
+    session["code"] = PLACEHOLDER_CODE
+    return redirect(url_for("code"))
+
+
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000)
+    app.run("0.0.0.0", port=5000, debug=True)

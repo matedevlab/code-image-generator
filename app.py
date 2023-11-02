5d73e2e37c6d68e4dc09f731898ddc12fc7b2e4a
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-02 20:25:15 +0100

    create image route

--- app.py
+++ app.py
@@ -71,5 +71,13 @@ def save_style():
     return redirect(url_for("style"))
 
 
+@app.route("/image", methods=["GET"])
+def image():
+    context = {
+        "message": "Done! 🎉",
+    }
+    return render_template("image.html", **context)
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)

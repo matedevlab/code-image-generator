05ec4872647d7e5f81018be12fceed661cd84c25
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-03 01:09:24 +0100

    add content to image route and refactor

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import config
+import base64
 from flask import (
     Flask,
     redirect,
@@ -10,14 +12,14 @@ from pygments import highlight
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import Python3Lexer
 from pygments.styles import get_all_styles
-
-import config
+from utils import take_screenshot_from_url
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 PLACEHOLDER_CODE = "print('Hello, World!')"
 DEFAULT_STYLE = "monokai"
+NO_CODE_FALLBACK = "# No Code Entered"
 
 
 @app.route("/", methods=["GET"])
@@ -36,7 +38,7 @@ def code():
 
 @app.route("/save_code", methods=["POST"])
 def save_code():
-    session["code"] = request.form.get("code")
+    session["code"] = request.form.get("code") or NO_CODE_FALLBACK
     return redirect(url_for("code"))
 
 
@@ -55,6 +57,7 @@ def style():
     context = {
         "message": "Select Your Style 🎨",
         "all_styles": list(get_all_styles()),
+        "style": session["style"],
         "style_definitions": formatter.get_style_defs(),
         "style_bg_color": formatter.style.background_color,
         "highlighted_code": highlight(session["code"], Python3Lexer(), formatter),
@@ -67,17 +70,25 @@ def save_style():
     if request.form.get("style") is not None:
         session["style"] = request.form.get("style")
     if request.form.get("code") is not None:
-        session["code"] = request.form.get("code")
+        session["code"] = request.form.get("code") or NO_CODE_FALLBACK
     return redirect(url_for("style"))
 
 
 @app.route("/image", methods=["GET"])
 def image():
+    session_data = {
+        "name": app.config["SESSION_COOKIE_NAME"],
+        "value": request.cookies.get(app.config["SESSION_COOKIE_NAME"]),
+        "url": request.host_url,
+    }
+    target_url = request.host_url + url_for("style")
+    image_bytes = take_screenshot_from_url(target_url, session_data)
     context = {
         "message": "Done! 🎉",
+        "image_b64": base64.b64encode(image_bytes).decode("utf-8"),
     }
     return render_template("image.html", **context)
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000, debug=True)
+    app.run("0.0.0.0", port=5000)

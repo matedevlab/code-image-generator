53701c7305d83af5f636ae6a007b5d4ad91b604e
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-03 01:05:54 +0100

    create config.py with secret key

--- setup.sh
+++ setup.sh
@@ -7,7 +7,10 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Install dependencies from requirements.txt
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Install Playwright browser binaries
-python -m playwright install
+python3 -m playwright install
+
+# Create a config.py file with SECRET_KEY
+echo "SECRET_KEY = '$(python -c 'import secrets; print(secrets.token_hex(16))')'" > config.py

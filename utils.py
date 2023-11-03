7e24c4952e1cba02393485a1653c472032a9d268
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-03 01:27:19 +0100

    fix: use chromium instead of webkit

--- utils.py
+++ utils.py
@@ -3,12 +3,12 @@ from playwright.sync_api import sync_playwright
 
 def take_screenshot_from_url(url, session_data):
     with sync_playwright() as playwright:
-        webkit = playwright.webkit
-        browser = webkit.launch()
+        browser = playwright.chromium.launch()
         browser_context = browser.new_context(device_scale_factor=2)
         browser_context.add_cookies([session_data])
         page = browser_context.new_page()
         page.goto(url)
-        screenshot_bytes = page.locator(".code").screenshot()
+        page.wait_for_selector(".code")
+        screenshot_bytes = page.screenshot()
         browser.close()
         return screenshot_bytes

b6f06b3f392a6cb20d2825bf8469acfb2e410fe9
kovacsmate88 <kovacsmate321@gmail.com>
2023-11-29 17:31:58 +0100

    set up pythonpath with setup.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 # Create a virtual environment named 'venv'
 python3 -m venv venv
 
-# Activate the virtual environment
-source venv/bin/activate
-
 # Install dependencies from requirements.txt
-pip3 install -r requirements.txt
+venv/bin/pip3 install -r requirements.txt
 
 # Install Playwright browser binaries
-python3 -m playwright install
+venv/bin/python3 -m playwright install
 
 # Generate a secret key and write it to .env file
-echo "SECRET_KEY=$(python -c 'import secrets; print(secrets.token_hex(16))')" > .env
+echo "SECRET_KEY=$(venv/bin/python -c 'import secrets; print(secrets.token_hex(16))')" > .env
+
+# Append PYTHONPATH setting to the virtual environment activation script
+echo "export PYTHONPATH=\$PYTHONPATH:$(pwd)" >> venv/bin/activate

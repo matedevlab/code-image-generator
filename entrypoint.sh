82dcdc980711daca053e056af0cb04b08b67cf97
kovacsmate88 <kovacsmate321@gmail.com>
2023-12-05 01:27:04 +0100

    fix: start gunicorn command in entrypint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 # Check if SECRET_KEY is not set
 if [ -z "$SECRET_KEY" ]; then
   echo "Generating a new secret key..."
-  export SECRET_KEY=$(python -c 'import secrets; print(secrets.token_hex(16))')
+  export SECRET_KEY=$(python3 -c 'import secrets; print(secrets.token_hex(16))')
 fi
 
 # Default to 2 workers if not set
 : ${WORKERS:=2}
 
 # Start Gunicorn
-exec gunicorn -k gevent -w ${WORKERS} -b 0.0.0.0:5000 app:app
+exec gunicorn -k gevent -w ${WORKERS} -b 0.0.0.0:5000 application.app:app
